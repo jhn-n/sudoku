25025b3d2640f824c64c64764025c81fa96fb201
jhn-n <182117243+jhn-n@users.noreply.github.com>
2025-07-03 12:32:45 +0100

    Use bitwise operations for notes

--- src/cell-class.js
+++ src/cell-class.js
@@ -1,33 +1,40 @@
 export class Cell {
     constructor() {
         this.value = null;
-        this.notes = Array(9).fill(true);
+        this.notes = 511;
     }
 
     setValue(n) {
         console.assert(n >= 1 && n <= 9, "Error - trying to set cell value out of range");
         this.value = n;
-        this.notes.fill(false);
+        this.notes = 0;
     }
 
     hasNote(n) {
-        return this.notes[n - 1];
+        return this.notes & (1 << (n - 1));
     }
 
     removeNote(n) {
-        this.notes[n - 1] = false;
+        this.notes &= ~(1 << (n - 1));
     }
 
     addNote(n) {
-        this.notes[n - 1] = true;
+        this.notes |= 1 << (n - 1);
     }
 
     reset() {
         this.value = null;
-        this.notes.fill(true);
+        this.notes = 511;
     }
 
     get noteCount() {
-        return this.notes.reduce((n, note) => (note ? n + 1 : n), 0);
+        let count = 0;
+        let x = this.notes;
+        while (x) {
+            count++;
+            x &= x - 1;
+        }
+        return count;
     }
 }
+
